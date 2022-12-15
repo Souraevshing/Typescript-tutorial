86f101a88f5050deaa0c2f8d5df135231e7f39d6
Souraevshing <isauravshing@gmail.com>
2022-12-15 14:02:34 +0530

    interface types enum implemented

--- build/app.js
+++ build/app.js
@@ -1,2 +1,14 @@
-export class App {
-}
+var ContactStatus;
+(function (ContactStatus) {
+    ContactStatus["Active"] = "active";
+    ContactStatus["Inactive"] = "inactive";
+    ContactStatus["New"] = "new";
+})(ContactStatus || (ContactStatus = {}));
+let contact = {
+    id: 1,
+    dob: new Date('20/03/1999'),
+    address: 'kahalgaon',
+    region: 'bhagalpur',
+    pinCode: 813204,
+    status: ContactStatus.Active,
+};
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,32 @@
-export class App {
+interface Contact extends Address {
   id: number
-  name: string
-  email: string
+  email?: string
+  name?: ContactName
+  dob: Date
+  status: ContactStatus
 }
+
+interface Address {
+  address: string
+  region: string
+  pinCode: number
+  state?: string
+  country?: string
+}
+
+enum ContactStatus {
+  Active = 'active',
+  Inactive = 'inactive',
+  New = 'new',
+}
+
+let contact: Contact = {
+  id: 1,
+  dob: new Date('20/03/1999'),
+  address: 'kahalgaon',
+  region: 'bhagalpur',
+  pinCode: 813204,
+  status: ContactStatus.Active,
+}
+
+type ContactName = string
